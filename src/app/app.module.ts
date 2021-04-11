293de804ec651ba9890b0375659fb2babfef3881
lucianoext <luciano.babaglio@gmail.com>
2021-04-11 18:13:03 -0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,11 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes=[
   {path:'products', component: ProductListComponent},
-  {path:'product/:id', component: ProductPageComponent}
+  {path:'product/:id', component: ProductPageComponent},
   {path:'', redirectTo: 'products', pathMatch: 'full' }
 ];
 
@@ -29,7 +29,7 @@ const appRoutes: Routes=[
     BrowserModule,
     FormsModule,
     HttpModule,
-    
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
