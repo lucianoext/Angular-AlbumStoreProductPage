cd660c691a9881df0cb54df403d655b9a10c4aa3
lucianoext <luciano.babaglio@gmail.com>
2021-04-11 14:39:48 -0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+
 import { Http, Response} from '@angular/http';
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import { AlbumStorePage } from '../../e2e/app.po';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,10 +10,9 @@ export class ProductService {
   private _albumUrl = '..assets/album.json';
 
   constructor(private _http: Http) { }
-  
+
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response) =>
-    response.json())
+    return this._http.get(this._albumUrl).map((response) => response.json())
     
   }
 
